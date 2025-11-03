bace5999c555f40675bfcf6ed5bd52c94de35ca6
ACST1728 <strombley1@gmail.com>
2025-11-03 23:10:33 +0000

    Create rugby_stats_app_v5_main_old.py

---
+++ rugby_stats_app_v5_main_old.py
@@ -0,0 +1,5 @@
+# Temporary routing to preserve your existing UI functions
+from rugby_stats_app_v5_main import (
+    page_players, page_metrics, page_hotkeys,
+    page_logger, page_reports, page_video, init_db
+)
